febd043606317e91b0b2c6938f262257e2941e67
Adam Miller <admiller@redhat.com>
2013-12-18 13:13:26 -0600

    kind of a dirty hack, but this fixes httpd across the board

---
+++ fixup_httpd_module.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#
+# Unfortunately as of httpd-2.4.6-2.fc19.armv7hl there is an issue with the 
+# mod_auth_digest module so we need to not load it until this issue is resolved
+#
+#
+# This script effectively does a quick/dirty clean up by just commenting it out
+# from being loaded.
+
+# Fix for httpd configs
+for i in $( \
+  grep -r 'LoadModule auth_digest_module modules/mod_auth_digest.so' \
+  /etc/httpd/ | awk -F: '{ print $1 }' );
+do  
+  sed -i 's/LoadModule auth_digest_module modules/mod_auth_digest.so/#LoadModule auth_digest_module modules/mod_auth_digest.so/g' $i;  
+done
+
+#Fix for rpm installed cartridges not in openshift cartridge repo
+for i in $( \
+  grep -r 'LoadModule auth_digest_module modules/mod_auth_digest.so' \
+  /usr/libexec/openshift/cartridges/ | awk -F: '{ print $1 }' );
+do  
+  sed -i 's/LoadModule auth_digest_module modules/mod_auth_digest.so/#LoadModule auth_digest_module modules/mod_auth_digest.so/g' $i;  
+done
+
+# Fix for cartridges installed in the openshift cartridge repo
+for i in $( \
+  grep -r 'LoadModule auth_digest_module modules/mod_auth_digest.so' \
+  /var/lib/openshift/.cartridge_repository/ | awk -F: '{ print $1 }' );
+do  
+  sed -i 's/LoadModule auth_digest_module modules/mod_auth_digest.so/#LoadModule auth_digest_module modules/mod_auth_digest.so/g' $i;  
+done
